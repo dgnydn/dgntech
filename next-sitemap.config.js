74b685452e18e47272bc92880d547c9d71e1643e
Doğan Aydın <dogan1aydin@gmail.com>
2022-10-23 23:22:30 +0300

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.dgntech.co',
+  generateRobotsTxt: true,
+  outDir: './public',
+  changefreq: 'daily',
+  priority: 0.7,
+}
