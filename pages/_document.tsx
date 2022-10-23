8f07dead9846ecf0f80c60519fc94d2dd38a1c24
Doğan Aydın <dogan1aydin@gmail.com>
2022-10-23 19:44:37 +0300

    new design

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,6 +8,19 @@ export default function Document() {
       <body className='bg-black/95'>
         <Main />
         <NextScript />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-078BG1G09L"></Script>
+        <Script id='gtag-init'
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-078BG1G09L', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </body>
     </Html>
   )
